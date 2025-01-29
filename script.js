a1dc43fe7c0181f23eaaded5f3882910812bac38
mildoss <vlad.maslo.05@gmail.com>
2025-01-29 23:41:31 +0200

    pizdec

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-const btn = document.querySelector('.btn')
-const images = document.querySelectorAll('.img')
+const btn = document.querySelector('.btn');
+const images = document.querySelectorAll('.img');
+
+const originalImages = ['andrey.jpg', 'image2.jpg'];
+const newImages = ['andrey2.jpg', 'images3.jpg'];
+
+let isOriginal = true;
 
 btn.addEventListener('click', (e) => {
+
     images.forEach(img => img.classList.add('hidden'));
 
     setTimeout(() => {
-        images[0].src = 'andrey2.jpg';
+        if (isOriginal) {
+            images[0].src = newImages[0];
+            images[1].src = newImages[1];
+        } else {
+            images[0].src = originalImages[0];
+            images[1].src = originalImages[1];
+        }
 
-        images[1].src = 'images3.jpg';
+        isOriginal = !isOriginal;
 
         images.forEach(img => img.classList.remove('hidden'));
     }, 500);
