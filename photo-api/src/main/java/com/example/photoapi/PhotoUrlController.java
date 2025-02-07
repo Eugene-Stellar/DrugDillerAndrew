a222f429cc865c6d51c0a754c739c63dcb754a79
Eugene <bielichenko777eugene@gmail.com>
2025-02-08 00:08:14 +0200

    Was added permissions which allow access for all domain

--- photo-api/src/main/java/com/example/photoapi/PhotoUrlController.java
+++ photo-api/src/main/java/com/example/photoapi/PhotoUrlController.java
@@ -6,6 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    // Разрешаем доступ для всех доменов
+    registry.addMapping("/**") // Применить для всех эндпоинтов
+        .allowedOrigins("*") // Разрешить доступ с любых доменов
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Разрешенные методы
+        .allowedHeaders("*") // Разрешить любые заголовки
+        .exposedHeaders("Authorization"); // При необходимости, чтобы указать, что возвращаем в заголовках
+  }
+}
+
 @RestController
 public class PhotoUrlController {
 
