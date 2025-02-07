020df0282863a0fe911a3b5f172507b0e3edb9b3
mildoss <vlad.maslo.05@gmail.com>
2025-02-08 00:50:09 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('.btn');
 const images = document.querySelectorAll('.img');
+const image = document.querySelectorAll('.test')[0];
 const container = document.querySelector('.container');
 const secret = document.querySelector('.secret');
 
@@ -7,6 +8,21 @@ const originalImages = ['andrey.jpg', 'image2.jpg'];
 const newImages = ['andrey2.jpg', 'images3.jpg'];
 
 let isOriginal = true;
+
+const img = document.createElement('img');
+const test = async () => {
+    try {
+        const response = await fetch('https://drugdillerandrew-production.up.railway.app/photo?number=1');
+        const data = await response.json();
+        console.log(data)
+        image.src = data.imageUrl;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
 
 secret.addEventListener('click', () => {
     container.classList.toggle('hide');
@@ -16,6 +32,8 @@ secret.addEventListener('click', () => {
 });
 
 btn.addEventListener('click', (e) => {
+
+    test()
 
     images.forEach(img => img.classList.add('hidden'));
 
