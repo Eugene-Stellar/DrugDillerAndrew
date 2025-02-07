0a7374feec7a2cec217720b748cd7dab61f34cac
Eugene <bielichenko777eugene@gmail.com>
2025-02-07 14:07:15 +0200

    1-st creation of backend/api.

---
+++ photo-api/src/test/java/com/example/photoapi/PhotoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
